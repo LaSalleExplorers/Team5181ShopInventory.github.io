1e0c1c4f7b50c6f5566cb2e6dd4b5c3a7c58414e
Team5181ShopInventory <frc5181@gmail.com>
2017-07-11 15:34:54 -0400

    Add files via upload

---
+++ constructors.js
@@ -0,0 +1,42 @@
+function part(partName,partNumber,partLocation,partLink,partCategory,metaTags){
+	this.name=partName;
+	this.num=partNumber;
+	this.location=partLocation;
+	this.ref=partLink;
+	this.category=partCategory;
+	this.tags=metaTags;
+	this.showOnMap=function(){
+		
+	}
+	this.addToTable=function(){
+		var newPartRow=document.createElement("tr");
+		var newNameNode=document.createElement("td");
+		var newNumberNode=document.createElement("td");
+		var newLocationNode=document.createElement("td");
+		var newLinkNode=document.createElement("td");
+		var newPartName=document.createTextNode(this.name);
+		var newPartNumber=document.createTextNode(this.num);
+		var newPartLocation=document.createTextNode(this.location);
+		var newPartLinkText=document.createTextNode(String(this.ref));
+		var newPartLink=document.createElement("a");
+		newPartRow.id=this.name;
+		newPartLink.href=this.ref;
+		newPartLink.target="_blank";
+		console.log(this.ref);
+		console.log(newPartLink.href);
+		newLocationNode.addEventListener("click",this.showOnMap());
+		newNameNode.appendChild(newPartName);
+		newNumberNode.appendChild(newPartNumber);
+		newLocationNode.appendChild(newPartLocation);
+		/*
+		newLinkNode.appendChild(newPartLink);
+		newLinkNode.appendChild(newPartLinkText);
+		*/
+		newPartLink.appendChild(newPartLinkText);
+		newPartRow.appendChild(newNameNode);
+		newPartRow.appendChild(newNumberNode);
+		newPartRow.appendChild(newLocationNode);
+		newPartRow.appendChild(newPartLink);
+		document.getElementById("registryDisplay").appendChild(newPartRow);
+	}
+}
---
+++ methods.js
@@ -0,0 +1,38 @@
+function searchRegistry(){
+	var searchValue=document.getElementById("searchBar").value;
+	console.log(searchValue);
+	if(searchValue!="Search Part Name or Part #"){
+		var resultsTable=document.createElement("table");
+		for(var i=0;i<registryData.length;i++){
+			var partObject=registryData[i];
+			console.log("name",partObject.name);
+			Loop1:
+			for(partProperty in partObject){
+				console.log("partProperty",partProperty);
+				if(partObject[partProperty]==searchValue){
+					var partRow=document.getElementById(partObject.name);
+					console.log(partObject);
+					resultsTable.appendChild(partRow);
+					break Loop1;
+				}
+				for(var j=0;j<partObject.tags.length;j++){
+					if(partObject.tags[j]==searchValue){
+						var partRow=document.getElementById(partObject.name);
+						console.log(partObject);
+						resultsTable.appendChild(partRow);
+						break Loop1;
+					}
+				}
+			}
+		}
+		resultsTable.id="resultsTable";
+		document.body.insertBefore(resultsTable,document.getElementById("registryDisplay"));
+		document.getElementById("resultsTable").style.visibility="visible";
+	}
+}
+function displayRegistry(){
+	for(var i=0;i<registryData.length;i++){
+		registryData[i].addToTable();
+	}
+	document.getElementById("registryDisplay").style.visibility="visible";
+}
